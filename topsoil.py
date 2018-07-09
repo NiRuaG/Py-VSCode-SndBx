c2edca51d9426b72d4b80c74ab3c0337313a85a4
NiRuaG <gaurin1107[at]gmail[dot]com>
2018-07-08 23:04:03 -0500

    harvest_at func

--- topsoil.py
+++ topsoil.py
@@ -1,6 +1,7 @@
 import random
 from collections import deque
 from collections import namedtuple
+from itertools import cycle
 import colorama as colo
 
 colo.init()#autoreset=True)
@@ -11,6 +12,11 @@ colo.init()#autoreset=True)
 class game_Topsoil():
     Garden = namedtuple('Garden', 'plants soil')
     Harvest_Action = 'A'
+    soil_cycle = {
+        'y': 'g',
+        'g': 'b',
+        'b': 'y'
+    }
 
     _bg_colors = {
         'y': colo.Back.LIGHTYELLOW_EX,
@@ -27,43 +33,49 @@ class game_Topsoil():
     def __init__(self, in_garden, in_cur_queue, in_future_queue=[]):
         print(colo.Fore.BLACK + colo.Back.WHITE, end='') # default bg/fg colors -- eventually put this to a seperate output class/method for printing game
 
-        self._garden = self.Garden(plants=list(in_garden[0]), soil=list(in_garden[1]))
-        self._garden_areas = self._init_search_areas()
+        self._garden = game_Topsoil.Garden(plants=list(in_garden[0]), soil=list(in_garden[1]))
+        
+        #self._garden_areas = self._init_search_areas()
+        self._timed_plants = []
 
         self._plant_queue = deque(in_cur_queue[:4])#, maxlen=4) doesn't seem necessary because we pop before adding - iow: it'll stay bounded to 4 anyway
+        self._plant_queue.append(game_Topsoil.Harvest_Action)
+
         self._future_que = list(in_future_queue)
+        self._score = 0
         
 
-    def _init_search_areas(self):
-        ret = [None for _ in range(16)]
-        visited = set()
-        area_id = 0
-        for seed_coord in range(16): 
-            if ret[seed_coord] in visited: continue 
+    # def _init_search_areas(self):
+    #     ret = [None for _ in range(16)]
+    #     area_id = 0
+    #     for start_area_coord in range(16): 
+    #         if ret[start_area_coord]: continue # if not None, the coordinate has an assigned area
 
-            # start of a new component area
-            area_id += 1
-            this_area = [seed_coord]
+    #         # start of a new component area 
+    #         area_id += 1
             
-            #dfs search for the whole component that matches the soil
-            match = self._garden.soil[seed_coord]
-            stack = [seed_coord]
-            while stack: 
-                this_coord = stack.pop()
-                if this_coord in visited: continue
-                visited.add(this_coord)
-                if self._garden.soil[this_coord] == match: this_area.append(this_coord)
-                stack.extend(self._coord_neighbors[this_coord])
+    #         #search for contiguous area that matches the soil color
+    #         match_soil = self._garden.soil[start_area_coord]
             
-            for c in this_area: ret[c] = area_id
+    #         dfs_stack = [start_area_coord]
+    #         visited = set()
+    #         while dfs_stack: 
+    #             cur_coord = dfs_stack.pop()
+    #             if ret[cur_coord] or cur_coord in visited: continue
+    #             visited.add(cur_coord)
+    #             if self._garden.soil[cur_coord] != match_soil: continue
+                
+    #             ret[cur_coord] = area_id
+    #             dfs_stack.extend(game_Topsoil._coord_neighbors[cur_coord])
             
-        return ret
+    #     return ret
 
     def pprint(self, extras=False, post=''):
+        print(self._score)
         print(*self._plant_queue, sep='')
         for row in zip(*[iter(zip(self._garden.plants, self._garden.soil))]*4):
             for cell in row:
-                print(self._bg_colors[cell[1]] + cell[0], end='')
+                print(game_Topsoil._bg_colors[cell[1]] + cell[0], end='')
             print() #newline
         #if extras: print extra info about areas
         print(colo.Back.WHITE + post, end='')
@@ -71,36 +83,70 @@ class game_Topsoil():
     def action_at(self, *action_coords): 
         for coord in action_coords:
             ###TODO: validate coordinate as possible plant or harvest location 
-            queued_action = self._plant_queue[0]
-
-            if (queued_action != self.Harvest_Action):
+            if (self._plant_queue[0] != game_Topsoil.Harvest_Action):
                 self._plant_at(coord)
+                if (self._plant_queue[0] == game_Topsoil.Harvest_Action):
+                    self._plant_queue.extend(self.next_plants_in_queue())
             else:
                 self._harvest_at(coord)
-                self._plant_queue.extend(self.next_plants_in_queue())
-                self._plant_queue.append(self.Harvest_Action)
+                self._plant_queue.append(game_Topsoil.Harvest_Action)
 
     def _plant_at(self, coord):
-        self._garden.plants[coord] = self._plant_queue.popleft()
+        plant = self._plant_queue.popleft()
+        self._garden.plants[coord] = plant
+        if plant == '3':
+            self._timed_plants.append(coord)
+
 
     def _harvest_at(self, coord):
+        ###TODO: check if coord is valid harvest (ie has plant)
+
+        self._plant_queue.popleft()
+
+        #find contigous area that matches the soil color & plant
+        area = []
+        match_soil = self._garden.soil[coord]
+        match_plant = self._garden.plants[coord]
+
+        dfs_stack = [coord]
+        visited = set()
+        while dfs_stack: 
+            cur_coord = dfs_stack.pop()
+            if cur_coord in visited: continue
+            visited.add(cur_coord)
+            if (   self._garden.soil[cur_coord]   != match_soil 
+                or self._garden.plants[cur_coord] != match_plant): 
+                continue
+            
+            area.append(cur_coord)
+            dfs_stack.extend(game_Topsoil._coord_neighbors[cur_coord])
+
+        #'remove' plants & cycle the soil under those plants
+        next_soil = game_Topsoil.soil_cycle[match_soil]
+        for coord in area:
+            self._garden.plants[coord] = ' ' ### ? None ?
+            self._garden.soil[coord] = next_soil
+
         #find contigous area of plants at coord
         #remove plants & cycle the soil under those plants
         #score
         pass 
+
+        # Score
+        if (match_plant in ('|', '*', 'w')):
+            self._score += ( len(area) * (len(area)+1) ) // 2
+            #print(self._score)
+        ### |, * = 1 + 2 + 3 + ..
     
-    def _top_soil_at(self, coord):
-        return self._garden.soil[coord]
+    # def _top_soil_at(self, coord):
+    #     return self._garden.soil[coord]
     
     def _no_moves(self):
         #return true/false
         pass
 
-    def _cycle_soil(self):
-        pass 
-    
     def next_plants_in_queue(self):
-        #TODO? handle problem for if future_que not a multiple of 3
+        ###TODO? handle problem for if future_que not a multiple of 3
         if not self._future_que:
             return list(input('What is the next set of plants in queue? '))
         else:
@@ -119,9 +165,20 @@ g = game_Topsoil(
                 "yggy"
                 "ygyy")),
 
-    in_cur_queue="|w|"+game_Topsoil.Harvest_Action,
-    in_future_queue="*w|")
+    in_cur_queue="|w|",
 
-g.pprint(post='\n')
-g.action_at(6,12,3,3)
+    in_future_queue=("*w|**|*ww*|*||www*||ww||||3")
+    )
+
+g.pprint(post='\n',extras=True)
+g.action_at(
+    6,12,3,3,
+    15,4,3,14,
+    9,14,7,11,
+    10,1,5,5,
+    12,1,8,13,
+    4,5,12,2,
+    8,13,11,11,
+    3,7,9,7,
+    4,3,7,12)
 g.pprint()
